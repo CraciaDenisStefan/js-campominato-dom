8929b8d8a6215ec2edc3af7d9967b2f6e40e88ea
CraciDenisStefan <crdenis13@gmail.com>
2023-05-29 16:19:50 +0200

    [FEAT] Creato array random + quadrati rossi.

--- js/script.js
+++ js/script.js
@@ -15,35 +15,63 @@ function createSquareElement(livello) {
 
     return square;
   }
+
+
+
+function randomNumberBombe(min, max){
+    return Math.floor(Math.random()*(max - min +1)+min)
+}
+
+
+
+
+
 //Mettiamo l'inizio del pulsante 
 start.addEventListener(`click`, function(){
-//Creiamo una function per determinare il numero di quadrati in base alla scelta del utente 
-    function level(){
-        let level = document.getElementById(`level`).value;
-            if(level === `Easy`){
-                 level = 100
-                }else if(level === `Medium`){
-                 level = 81
-                }else if(level === `Hard`){
-                  level = 49
-                }
-        return level
-    }
+   //Creiamo level per determinare il numero di quadrati in base alla scelta del utente 
+    let level = document.getElementById(`level`).value;
+        if(level === `Easy`){
+             level = 100
+            }else if(level === `Medium`){
+             level = 81
+            }else if(level === `Hard`){
+              level = 49
+            }
+        //Creiamo un array di 16 numeri random che non si ripetano
+            let bombe = [];
+            console.log(bombe)
+            while (bombe.length < 16) {
+              let bombNumber = randomNumberBombe(1, level);
+              if (!bombe.includes(bombNumber)) {
+                bombe.push(bombNumber);
+              }
+            }
+            
+             
+
+
 //Prendiamo il container del main e li creiamo una classe in modo che si apra insieme al pulsante
     let container = document.querySelector(`.container_main`)
     container.classList.add(`container`)
 //Svuotiamo il container ad ogni click per inserire i nuovi dati della nuova difficoltà
     container.innerHTML = '';
 //Creiamo il ciclo che si comportera di conseguenza alle scelte precedenti 
-    for (let i=0; i<level(); i++){
+    for (let i=0; i<level; i++){
         let square = createSquareElement(document.getElementById('level').value);
         square.innerText = i + 1;
+
 //Mettiamo in pagina i risultati 
         container.append(square)
 //Facciamo uscire nella console il quadrato con il numero cliccato dal utente e cambiamo il background del quadrato (senza poi ritornare al colore iniziale ricliccando) 
     square.addEventListener(`click`, function(){
+        if(!bombe.includes(square.innerText = i + 1)){
         this.classList.add(`clicked`)
-        console.log(this);
+        }else{
+            this.classList.add(`bomb`)
+        }
+        console.log(this.innerText)
+
     })
     }
+
 })
